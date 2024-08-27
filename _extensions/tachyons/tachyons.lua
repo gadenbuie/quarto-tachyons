9e07e94df6f51a2db30f51ad8a2fe08494f375fe
Garrick Aden-Buie <garrick@adenbuie.com>
2024-08-27 08:26:23 -0400

    🩹 use Pandoc filter to add dependency

--- _extensions/tachyons/tachyons.lua
+++ _extensions/tachyons/tachyons.lua
@@ -1,7 +1,9 @@
-if quarto.doc.is_format("html:js") then
-  quarto.doc.add_html_dependency({
-    name = 'tachyons',
-    version = '4.12.0',
-    stylesheets = { "tachyons.css" },
-  })
+function Pandoc()
+  if quarto.doc.is_format("html:js") then
+    quarto.doc.add_html_dependency({
+      name = 'tachyons',
+      version = '4.12.0',
+      stylesheets = { "tachyons.css" },
+    })
+  end
 end
